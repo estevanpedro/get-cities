8621bae426c7a657cc29467c7ee3a27c8417911c
Estevan Wisoczynski <estevanpwr@outlook.com>
2021-01-28 10:05:57 -0300

    update

---
+++ app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import csv
+
+app = FastAPI()
+
+@app.get("/city/{country}/{region}")
+async def city(country, region):
+    with open('world.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        list_of_cities = ['Other city']
+        for row in reader:
+            if row['country'] == country:
+                if row['subcountry'] == region:
+                    list_of_cities.append(row['name'])
+        return list_of_cities
