5de26cdeeaf75f1e68f15e3875f2d619d7aec7db
Estevan Wisoczynski <estevanpwr@outlook.com>
2021-01-28 10:30:03 -0300

    fix params

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import csv
 app = FastAPI()
 
 
-@app.get("/city/{country}/{region}")
-async def city(country, region):
+@app.get("/cities")
+async def cities(country, region):
     with open('world.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         list_of_cities = ['Other city']
